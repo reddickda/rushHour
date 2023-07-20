8d842d482090d48e88465f0a0acf064f5350068d
David <david@sirrus7.com>
2023-07-20 18:38:49 -0400

    amazon link

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ function App() {
       <button onClick={handleClickSolve}>Solve it!</button>
       <button onClick={() => clearBoard()}>Clear board</button>
       {solvedBoard ? <div style={{borderRadius: 5, backgroundColor: 'green'}}><div>solved!</div> <Board pathToDisplay={solvedBoard} /></div> : <div>not solved...</div>}
+      <div><a target="_blank" href="https://www.amazon.com/gp/search?ie=UTF8&tag=dreddick-20&linkCode=ur2&linkId=9ef5790b3be96d5ea3b119733363d71d&camp=1789&creative=9325&index=toys-and-games&keywords=rushhourgame">Purchase your own Rush Hour game here!</a></div>
     </div>
   )
 }
